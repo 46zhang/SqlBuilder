eef0a56e2ec2a43fd7a11e323fea916522a95bbb
zhangshinan <zhangshinan@huya.com>
2020-06-17 15:24:53 +0800

    修复了之前的Bug

--- sqlorm/orm.py
+++ sqlorm/orm.py
@@ -4,21 +4,30 @@ import logging
 import aiomysql
 
 
-async def create_pool(**kw):
+async def create_pool(loop, **kw):
     logging.info('create database connection pool...')
     global __pool
     __pool = await aiomysql.create_pool(
-        host=kw.get('host', 'localhost'),
-        port=kw.get('port', 3306),
-        user=kw['user'],
-        password=kw['password'],
-        db=kw['db'],
-        charset=kw.get('charset', 'utf8'),
-        autocommit=kw.get('autocommit', True),
-        maxsize=kw.get('maxsize', 10),
-        minsize=kw.get('minsize', 1),
-        loop=asyncio.get_event_loop()
+        host='127.0.0.1',
+        port=3306,
+        user='pusher',
+        password='push',
+        db='sqlorm',
+        loop=loop
     )
+    print(__pool)
+    # __pool = await aiomysql.create_pool(
+    #     host=kw.get('host', 'localhost'),
+    #     port=kw.get('port', 3306),
+    #     user=kw['user'],
+    #     password=kw['password'],
+    #     db=kw['db'],
+    #     charset=kw.get('charset', 'utf8'),
+    #     autocommit=kw.get('autocommit', True),
+    #     maxsize=kw.get('maxsize', 10),
+    #     minsize=kw.get('minsize', 1),
+    #     loop=loop
+    # )
 
 
 async def select(sql: str, args, size=None):
--- sqlorm/test_orm.py
+++ sqlorm/test_orm.py
@@ -2,9 +2,6 @@ import asyncio
 import time
 import uuid
 
-import aiomysql
-import pymysql
-
 from sqlorm import orm
 from sqlorm.model import Model
 from sqlorm.field import StringField, FloatField, BooleanField
@@ -15,6 +12,7 @@ def next_id():
 
 
 class User(Model):
+    __table__ = 'users'
     id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
     email = StringField(ddl='varchar(50)')
     passwd = StringField(ddl='varchar(50)')
@@ -24,36 +22,15 @@ class User(Model):
     created_at = FloatField(default=time.time)
 
 
-def test_orm():
-    asyncio.run(
-        orm.create_pool( user='pusher', password='push', db='sqlorm'))
+async def test_example(loop):
+    await orm.create_pool(loop=loop, host='localhost', port=3306,
+                          user='pusher', password='push',
+                          db='sqlorm', autocommit=False)
     u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
-    a = "fafaf"
-    print(a)
-    print(u)
-    asyncio.run(u.save())
-    print("over")
+    await u.save()
 
-
-async def go():
-    pool = await aiomysql.create_pool(host='localhost', port=3306,
-                                      user='pusher', password='push',
-                                      db='sqlorm', loop=asyncio.get_event_loop(), autocommit=False)
-
-    with (await pool) as conn:
-        cur = await conn.cursor()
-        await cur.execute("SELECT 10")
-        # print(cur.description)
-        (r,) = await cur.fetchone()
-        assert r == 10
-    pool.close()
-    await pool.wait_closed()
-    # conn = await aiomysql.connect(host="localhost", port=3306,
-    #                               user="pusher", password="push",
-    #                               db="sqlorm", loop=asyncio.get_event_loop()
-    #                               )
 
 
 if __name__ == "__main__":
-    test_orm()
-    #asyncio.run(go(), debug=True)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(test_example(loop))
